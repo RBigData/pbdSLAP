79b01c44f2146319bf5adabfe4d5cf6a30212322
snoweye <wccsnow@gmail.com>
2014-06-21 16:23:13 -0400

    0.2-0

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-rm *.aux *.bbl *.blg *.log *.out *.toc
+R_HOME=`Rscript -e 'cat(R.home()[1])'`
+JSS_BST=${R_HOME}/share/texmf/bibtex/bst/jss.bst
+JSS_CLS=${R_HOME}/share/texmf/tex/latex/jss.cls
+
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
+cp ${JSS_BST} ./
+cp ${JSS_CLS} ./
+
 pdflatex pbdSLAP-guide.Rnw
 bibtex pbdSLAP-guide
 pdflatex pbdSLAP-guide.Rnw
 pdflatex pbdSLAP-guide.Rnw
 pdflatex pbdSLAP-guide.Rnw
-rm *.aux *.bbl *.blg *.log *.out *.toc
 
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
 mv -f *.pdf ../inst/doc/
 cp -f *.Rnw ../inst/doc/
