57a10a17174780ac8d5a667246860221a996f12b
wccsnow <wccsnow@gmail.com>
2019-04-04 23:08:44 -0400

    updates

--- R/get_conf.r
+++ R/get_conf.r
@@ -10,7 +10,18 @@ get.conf <- function(arg, arch = '', package = "pbdMPI"){
 
   id <- grep(paste("^", arg, " = ", sep = ""), ret)
   if(length(id) > 0){
-    cat(gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]]))
+    ret <- gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]])
+    if(arg == "MPI_LIB" && .Platform$OS.type == "windows" &&
+       length(grep("-L", ret)) == 0){
+      if(length(grep(".*pbdMPI/libs/x64/libmsmpi64.a", ret[1])) == 1){
+        ret <- tools::file_path_as_absolute(
+                 system.file("libs/x64/libmsmpi64.a", package = "pbdMPI"))
+      } else{
+        ret <- gsub("\\\\", "/", utils::shortPathName(ret))
+      }
+    }
+
+    cat(ret)
   } else{
     stop("The arg is not found.")
   }
